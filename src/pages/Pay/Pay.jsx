5719542882998ede4c599787f5cfec1a86a7ed8c
Umarov Rahmatillo <rahmatillo722022@gmail.com>
2024-08-27 20:22:10 +0500

    main

--- src/pages/Pay/Pay.jsx
+++ src/pages/Pay/Pay.jsx
@@ -1,15 +1,23 @@
 import { useTranslation } from "react-i18next"
 import "./Pay.css"
-
+import money from "../../assets/naqt.webp"
+import click from "../../assets/click.jpg"
+import payme from "../../assets/payme.jpg"
 const Pay = () => {
   const [t] = useTranslation("global");
   return (
     <div className='pay'>
       <h1>{t("payPage.pay")}</h1>
       <div className="payCards">
-        <div className="payCard">naqt</div>
-        <div className="payCard">click</div>
-        <div className="payCard">payme</div>
+        <div className="payCard">
+          <img src={money} alt="money" />
+        </div>
+        <div className="payCard">
+          <img src={click} alt="click" />
+        </div>
+        <div className="payCard">
+          <img src={payme} alt="payme" />
+        </div>
       </div>
     </div>
   )
